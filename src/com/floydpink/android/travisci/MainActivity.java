7da761f86f57d3380f044e999be77ff04d70ac06
Floyd Pink <imbleedingme@googlemail.com>
2012-10-16 00:17:58 -0400

    adding the new android project
    Signed-off-by: Floyd Pink <imbleedingme@googlemail.com>

---
+++ src/com/floydpink/android/travisci/MainActivity.java
@@ -0,0 +1,15 @@
+package com.floydpink.android.travisci;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
