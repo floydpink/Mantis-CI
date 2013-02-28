79a4e9a1d2f029ec77de36d6460f4e5c5f2343f3
Floyd Pink <imbleedingme@googlemail.com>
2013-02-27 22:43:21 -0500

    ember barely working
    Signed-off-by: Floyd Pink <imbleedingme@googlemail.com>

--- assets/www/js/script.js
+++ assets/www/js/script.js
@@ -1,2 +1,43 @@
-//TODO: Start using requirejs
-var Travis = {};
+var Travis = Ember.Application.create({
+    ready:function () {
+        console.log('Ember is ready');
+    }
+});
+
+Travis.MainView = Ember.View.extend({
+    attributeBindings:['data-role'],
+    'data-role':'page',
+    templateName:'main',
+    id:'main-view',
+    didInsertElement:function () {
+        console.log('main inserted');
+        console.log(this.$());
+        $.mobile.changePage(this.$());
+    }
+});
+
+$(document).on('mobileinit', function () {
+    console.log('in mobileinit event');
+    $.mobile.ajaxEnabled = false;
+    $.mobile.linkBindingEnabled = false;
+    $.mobile.hashListeningEnabled = false;
+    $.mobile.pushStateEnabled = false;
+
+    // Remove page from DOM when it's being replaced (if you use pages)
+    $('div[data-role="page"]').on('pagehide', function (event, ui) {
+        $(event.currentTarget).remove();
+    });
+});
+
+
+$('#splash').on('pageinit', function () {
+    console.log('in pageinit event');
+    var v = Travis.get('mainView');
+
+    if (!v) {
+        console.log('main not created');
+        v = Travis.MainView.create({});
+        Travis.set('mainView', v);
+        v.append();
+    }
+});
