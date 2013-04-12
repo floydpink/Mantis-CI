4462c1fe488f90dc6240bc5b09984926024084f7
Floyd Pink <imbleedingme@googlemail.com>
2013-04-12 01:52:44 -0400

    selectively injecting websocket into android using logic in grunt build
    Signed-off-by: Floyd Pink <imbleedingme@googlemail.com>
    [ci skip]

--- src/com/floydpink/android/travisci/MainActivity.java
+++ src/com/floydpink/android/travisci/MainActivity.java
@@ -5,25 +5,26 @@ import android.os.Bundle;
 import org.apache.cordova.*;
 
 import com.google.analytics.tracking.android.EasyTracker;
+import com.strumsoft.websocket.phonegap.WebSocketFactory;
 
-public class MainActivity extends DroidGap
-{
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        super.loadUrl("file:///android_asset/www/xp/index.html");
-    }
+public class MainActivity extends DroidGap {
 	@Override
-	public void onStart()
-	{
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		super.loadUrl("file:///android_asset/www/xp/index.html");
+
+		this.appView.addJavascriptInterface(new WebSocketFactory(this),
+				"WebSocketFactory");
+	}
+
+	@Override
+	public void onStart() {
 		super.onStart();
 		EasyTracker.getInstance().activityStart(this);
 	}
 
 	@Override
-	public void onStop()
-	{
+	public void onStop() {
 		super.onStop();
 		EasyTracker.getInstance().activityStop(this);
 	}
