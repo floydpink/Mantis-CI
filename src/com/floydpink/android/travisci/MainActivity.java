95f23a8bd15e5f1327ef658e85658a3f9d0e00b1
Floyd Pink <imbleedingme@googlemail.com>
2013-06-24 22:22:07 -0400

    added websocket support [deploy]
    Signed-off-by: Floyd Pink <imbleedingme@googlemail.com>

--- src/com/floydpink/android/travisci/MainActivity.java
+++ src/com/floydpink/android/travisci/MainActivity.java
@@ -5,16 +5,12 @@ import android.os.Bundle;
 import org.apache.cordova.*;
 
 import com.google.analytics.tracking.android.EasyTracker;
-import com.strumsoft.websocket.phonegap.WebSocketFactory;
 
 public class MainActivity extends DroidGap {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		super.loadUrl("file:///android_asset/www/xp/index.html");
-
-		this.appView.addJavascriptInterface(new WebSocketFactory(this),
-				"WebSocketFactory");
 	}
 
 	@Override
